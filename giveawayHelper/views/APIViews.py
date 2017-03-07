5d14d0b3c35a462b82991889cd48b5bc5c1e2e45
Wes Evans <euphwes@gmail.com>
2017-03-07 14:48:16 -0700

    Implemented optional text filtering and date filtering of comments, and now choosing a random winner from eligible comments.

--- giveawayHelper/views/APIViews.py
+++ giveawayHelper/views/APIViews.py
@@ -3,11 +3,17 @@ from django.views.decorators.csrf import csrf_exempt
 
 from ..settings import REDDIT_SECRET, REDDIT_CLIENT_ID
 
-import praw
+import praw, random
+from datetime import datetime
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-
+def comment_to_dict(comment):
+    return {
+        'user': '/u/{}'.format(comment.author.name),
+        'url': 'http://www.reddit.com/{}'.format(comment.permalink()),
+        'first_line': '{}...'.format(comment.body.splitlines()[0])
+    }
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -21,6 +27,16 @@ def run_thread_giveaway(request):
     except KeyError as e:
         return HttpResponseBadRequest("Forgot to submit a URL")
 
+    try:
+        date_limit = int(request.POST['date_limit'])
+    except KeyError as e:
+        date_limit = datetime.utcnow().timestamp()
+
+    try:
+        filter_text = request.POST['filter_text']
+    except KeyError as e:
+        filter_text = None
+
     reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
                          client_secret=REDDIT_SECRET,
                          user_agent='/r/cubers giveaway helper v0.1 (by /u/euphwes)')
@@ -32,24 +48,31 @@ def run_thread_giveaway(request):
         "eligible_comments": list(),
         "duplicate_user_comments": list(),
         "filtered_comments": list(),
-        "winner": dict()
+        "too_new_comments": list(),
+        "winner": None
     }
 
     eligible_users = dict()
 
-    for comment in thread.comments:
+    comments = thread.comments[:]
+    comments.sort(key=lambda x: x.created_utc, reverse=True)
+
+    for comment in comments:
+        if comment.created_utc > date_limit:
+            result['too_new_comments'].append(comment_to_dict(comment))
+            continue
+
+        if filter_text and filter_text in comment.body.splitlines()[0]:
+            result['filtered_comments'].append(comment_to_dict(comment))
+            continue
+
         if comment.author.name not in eligible_users:
             eligible_users[comment.author.name] = comment.id
-            result['eligible_comments'].append({
-                'user': '/u/{}'.format(comment.author.name),
-                'url': 'http://www.reddit.com/{}'.format(comment.permalink()),
-                'first_line': '{}...'.format(comment.body.splitlines()[0])
-            })
+            result['eligible_comments'].append(comment_to_dict(comment))
         else:
-            result['duplicate_user_comments'].append({
-                'user': '/u/{}'.format(comment.author.name),
-                'url': 'http://www.reddit.com/{}'.format(comment.permalink()),
-                'first_line': '{}...'.format(comment.body.splitlines()[0])
-            })
+            result['duplicate_user_comments'].append(comment_to_dict(comment))
+
+    if result['eligible_comments']:
+        result['winner'] = random.choice(result['eligible_comments'])
 
     return JsonResponse(result)
